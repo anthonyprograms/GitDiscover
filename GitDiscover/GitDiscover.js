33ee45b4af71846b9a55f5ce27c82fd012e151b5
Anthony <anthonygreen274@icloud.com>
2015-07-19 23:29:18 -0700

    Added user accounts and a more complete form functionality

--- GitDiscover/GitDiscover.js
+++ GitDiscover/GitDiscover.js
@@ -13,18 +13,22 @@ if (Meteor.isClient) {
 
       var name = $('input.name').val();
       var url = $('input.url').val();
-      var description = $('input.description').val();
+      var description = $('textarea.description').val();
+      var language = $('select.language').val();
+      var skillLevel = $('select.skill-level').val();
 
       Entries.insert({
         createdAt: new Date(),
         name: name,
         url: url,
-        description: description
+        description: description,
+        language: language,
+        skillLevel: skillLevel
       });
 
       $('input.name').val("");
       $('input.url').val("");
-      $('input.description').val("");
+      $('textarea.description').val("");
     }
   });
 }
