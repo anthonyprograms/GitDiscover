e51a18811c3b61d129acc3159c11d60478401e67
Anthony <anthonygreen274@icloud.com>
2015-07-20 23:22:27 -0700

    Progress on filter

--- GitDiscover/GitDiscover.js
+++ GitDiscover/GitDiscover.js
@@ -7,7 +7,14 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     entries: function () {
-      return Entries.find({}, {sort: {createdAt: -1}});
+      var level = Session.get("FilterSearch");
+      // console.log(levels.length);
+      if (level.length > 0){
+          return Entries.find({skillLevel: level});
+      }
+      else{
+        return Entries.find({}, {sort: {createdAt: -1}});
+      }
     }
   });
 
@@ -15,11 +22,17 @@ if (Meteor.isClient) {
     "submit .filters": function (event) {
       event.preventDefault();
 
-      // var skillLevel = $('filled-in:checked').val();
+      // var languages = [];
+      //   $('input[name=filter-level]:checked').each(function(i){
+      //     languages[i] = $(this).val();
+      //   });
 
-      // console.log(skillLevel);
+      var level = "";
+      $('input[name=filter-level]:checked').each(function(i){
+        level = $(this).val();
+      });
 
-      // return Entries.find({}, {sort: {createdAt}})
+      Session.set("FilterSearch", level);
     },
 
     "submit .new-entry": function (event) {
@@ -28,16 +41,13 @@ if (Meteor.isClient) {
       var name = $('input#name').val();
       var url = $('input#url').val();
       var description = $('textarea#description').val();
-      // var language = $('input.filled-in:checked').val();
       var skillLevel = $('select#skill-level').val();
-
       var languages = [];
-        $(':checkbox:checked').each(function(i){
+        $('input[name=form-language]:checked').each(function(i){
           languages[i] = $(this).val();
         });
-      console.log(languages);
 
-      if ((name === "") || (url === "") || (description === "")){
+      if ((name === "") || (url === "") || (description === "") || (skillLevel == "") || (languages.length === 0)){
         alert("You need to fill out each field before publishing your post.");
       }
       else{
@@ -62,6 +72,9 @@ if (Meteor.isClient) {
   Template.entry.events({
     "click .delete": function () {
       Entries.remove(this._id);
+    },
+    "click .update": function () {
+      // $('input#name').value = this.name;
     }
   });
 
--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 Discover open source projects that anyone of any level can contribute to
 
 ### To-Do:
-1. Hide/Show form
-2. Filter search
-3. GitHub API
-4. Comments
-5. Hide/Show filters
-6. Update Button
+1. Filter search
+2. GitHub API
+3. Comments
+4. Update Button
